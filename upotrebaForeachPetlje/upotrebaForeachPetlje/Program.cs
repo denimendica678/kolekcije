ecd75daa0bc1add92fa067ac6afa733efc396811
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-02-15 08:53:00 +0100

    Add files via upload

---
+++ upotrebaForeachPetlje/upotrebaForeachPetlje/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upotrebaForeachPetlje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> brojevi = new List<int>();
+
+            while (true)
+            {
+                Console.Write("Unesite cijeli broj:");
+                int broj = int.Parse(Console.ReadLine());
+
+                if (broj == 0)
+                {
+                    break;
+                }
+
+                brojevi.Add(broj);
+            }
+            brojevi.Sort();
+            brojevi.Reverse();
+
+
+            Console.WriteLine("Uneseni brojevi padajućim redoslijedom:");
+            foreach (int broj in brojevi)
+            {
+                Console.Write(broj + ", ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
